41a329f883c43fd456a937dfa11d6bce0045bd8f
John Hardy <jh@lagado.com>
2024-03-12 01:04:24 +1100

    jsdoc

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,68 @@
+//@ts-check
+
+/** @typedef {(url: string) => RouteData | null} Matcher */
+/** @typedef {Response | Promise<Response> | null} HandlerResponse */
+/** @typedef {Object} RouteTable */
+/** @typedef {Object} Params */
+
+/**
+ * @typedef {(
+*   request: Request,
+*   routeData: RouteData
+* ) => HandlerResponse} Handler
+*/
+
+/**
+* @typedef {Object} RouteData
+* @property {Handler} handler
+* @property {Params} params
+* @property {string} url
+*/
+
 const optionalParam = /\((.*?)\)/g;
 const namedParam = /(\(\?)?:\w+/g;
 const escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
 const splatParam = /\*/g;
 
+/**
+ * @param {string[]} [messages=[]]
+ * @param {string} [content="no content"]
+ * @returns {string}
+ */
+const layout = (messages = [], content = "no content") => `<!doctype html>
+<html lang="">
+<head>
+    <title>Contact App</title>
+    <link rel="stylesheet" href="https://the.missing.style/v0.2.0/missing.min.css">
+    <link rel="stylesheet" href="/static/site.css">
+    <script src="/static/js/htmx-1.8.0.js"></script>
+    <script src="/static/js/_hyperscript-0.9.7.js"></script>
+    <script src="/static/js/rsjs-menu.js" type="module"></script>
+    <script defer src="https://unpkg.com/alpinejs@3/dist/cdn.min.js"></script>
+</head>
+<body hx-boost="true">
+<main>
+    <header>
+        <h1>
+            <all-caps>contacts.app</all-caps>
+            <sub-title>A Demo Contacts Application</sub-title>
+        </h1>
+    </header>
+    ${messages.map((message) => `<div class="flash">${message}}</div>`).join()}
+    ${content}
+</main>
+</body>
+</html>`;
+
+/**
+ * @param {RouteTable} routes
+ * @returns {Matcher}
+ */
 const defineRoutes = (routes) => {
-  const routeList = Object.entries(routes).map(([key, value]) => {
+  const routeList = Object.entries(routes).map(([key, handler]) => {
     const namedParams = [];
-    pattern = key
+
+    const pattern = key
       .replace(escapeRegExp, "\\$&")
       .replace(optionalParam, "(?:$1)?")
       .replace(namedParam, (match, optional) => {
@@ -19,14 +75,13 @@ const defineRoutes = (routes) => {
       });
     const regExp = new RegExp("^" + pattern + "(?:\\?([\\s\\S]*))?$");
     return {
-      value,
+      handler,
       regExp,
       namedParams,
-      pattern: pattern,
     };
   });
   return (url) => {
-    for (const { value, regExp, namedParams, pattern } of routeList) {
+    for (const { handler, regExp, namedParams } of routeList) {
       const result = regExp.exec(url);
       if (result) {
         const params = result.slice(1, -1).reduce(function (acc, val, index) {
@@ -34,10 +89,9 @@ const defineRoutes = (routes) => {
           return acc;
         }, {});
         return {
-          value,
+          handler: handler,
           params,
           url,
-          pattern,
         };
       }
     }
@@ -45,35 +99,37 @@ const defineRoutes = (routes) => {
   };
 };
 
-self.addEventListener("install", function (event) {
-  // Skip the 'waiting' lifecycle phase, to go directly from 'installed' to 'activated', even if
-  // there are still previous incarnations of this service worker registration active.
+/** @type {any}  */
+const sw = self;
+sw.addEventListener("install", function (event) {
   console.log("Installing...");
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil(sw.skipWaiting());
   console.log("Install done");
 });
-
-self.addEventListener("activate", function (event) {
-  // Claim any clients immediately, so that the page will be under SW control without reloading.
+sw.addEventListener("activate", function (event) {
   console.log("Activating...");
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(sw.clients.claim());
   console.log("Activate done");
 });
-
-function isImage(fetchRequest) {
-  return fetchRequest.method === "GET" && fetchRequest.destination === "image";
+/**
+ * @param {Request} request
+ * @returns {boolean}
+ */
+function isImage(request) {
+  return request.method === "GET" && request.destination === "image";
 }
-
 let matcher;
-self.addEventListener("fetch", (e) => {
+sw.addEventListener("fetch", (e) => {
   if (!matcher) {
     matcher = defineRoutes({
+      "/x": () => {
+        return new Response(layout(["message 1"], "body content x"));
+      },
       "/contacts": async (request, { params, url }) => {
         const body =
           request.method === "POST"
             ? Object.fromEntries((await request.formData()).entries())
             : "<none>";
-
         return new Response(
           `list ${request.method} ${JSON.stringify(
             params
@@ -88,25 +144,22 @@ self.addEventListener("fetch", (e) => {
     });
   }
   const { request } = e;
+  if (!request) return;
   let response = null;
-  if (request.headers.get("Accept").includes("text/html")) {
+  if (request.headers.get("Accept")?.includes("text/html")) {
     const { pathname } = new URL(request.url);
     console.log("Fetching HTML ", request.method, pathname);
-    const match = matcher(pathname);
-    if (match) response = match.value(request, match);
+    const routeData = matcher(pathname);
+    if (routeData) response = routeData.handler(request, routeData);
   }
-
   if (isImage(request)) {
     response = fetch("/broken.png");
   }
-
   if (!response) {
     response = fetch(request);
   }
-
   e.respondWith(response);
 });
-
 // if (request.headers.get('Accept').includes('text/html')) {
 // if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript')) {
 // if (request.headers.get('Accept').includes('image') || request.url.includes('your-web-font')) {
